65202a154e605a51068cdf7d5cb98a70a5edcb59
goremi <ffdan@naver.com>
2023-09-04 22:02:22 +0900

    server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for, flash,request
+from flask.helpers import send_from_directory
+from flask_cors import CORS,cross_origin
+
+app = Flask(__name__,static_url_path='',static_folder='../client/build')
+CORS(app)
+@app.route('/')
+@cross_origin()
+def home():
+    return send_from_directory(app.static_folder,'index.html')
+@app.route('/api')
+@cross_origin()
+def api():
+    return {"members":['goremi','gogo','eromi']}
+@app.route('/data',methods=['GET','POST'])
+def memebrs():
+   if request.method == 'POST':
+       data =request.get_json()
+       print(data)
+       user =data['user']
+       return {"message":f"Received{user}"} 
+if __name__ == "__main__":
+    app.run(debug=True)
